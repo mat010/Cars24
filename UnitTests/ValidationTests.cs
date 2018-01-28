1c0ccac567d934fba78869edb92df5a1a6b3a5a9
Boguslaw Boczkowski <boguslaw.boczkowski@gmail.com>
2018-01-28 12:04:52 +0100

    Added unit tests for solution

---
+++ UnitTests/ValidationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic;
+using NUnit.Framework;
+using DataAccess;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class ValidationTests
+    {
+        [Test]
+        public void ShouldValidateNewCar()
+        {
+            var validator = new Validation();
+
+            var car = new CarDetailsVM();
+
+            bool isValid = validator.IsModelValid(car);
+
+            Assert.IsTrue(isValid);
+        }
+    }
+}
